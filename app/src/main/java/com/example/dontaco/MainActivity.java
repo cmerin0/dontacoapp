78b1f8b727c5bcea94a8224a1faa46a0e9cbc98e
cmerin0 <carlosmerino@gmail.com>
2021-04-10 17:48:46 -0600

    Agregando proyecto con Login y Splashscreen

---
+++ app/src/main/java/com/example/dontaco/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.dontaco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask(){
+            @Override
+            public void run(){
+                Intent i = new Intent(MainActivity.this, IndexActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(task, 5000);
+    }
+}
